e9aedc191389918b2130ce60c27460ae35f2f415
github username <github email address>
2019-07-18 10:07:14 -0400

    Done.

---
+++ models/puppy.rb
@@ -0,0 +1,19 @@
+class Puppy
+    attr_reader :breed
+    attr_accessor :months_old, :name
+
+    @@all = []
+    
+    def initialize(name, breed, months_old)
+        @name = name
+        @breed = breed
+        @months_old = months_old
+        @@all << self
+    end
+    
+    def all
+        @@all
+    end
+
+
+end
